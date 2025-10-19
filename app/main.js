2bc538f7587382b40ac7d5c9902777598cf02f1b
KB <karanbhaskar@KBs-MacBook-Air.local>
2025-10-18 22:58:29 -0400

    Test-1c

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 if (pattern.length === 1) {
     function matchPattern(inputLine, pattern) {
     return inputLine.includes(pattern);
-  } }else {
+  }}else {
     throw new Error(`Unhandled pattern ${pattern}`);
   }
 
@@ -19,11 +19,11 @@ function main() {
   console.error("Logs from your program will appear here");
 
   // Uncomment this block to pass the first stage
-  // if (matchPattern(inputLine, pattern)) {
-  //   process.exit(0);
-  // } else {
-  //   process.exit(1);
-  // }
+  if (matchPattern(inputLine, pattern)) {
+      process.exit(0);
+    } else {
+        process.exit(1);
+      }
 }
 
 main();
